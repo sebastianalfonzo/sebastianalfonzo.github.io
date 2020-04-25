287dc8771f8504fe3e44385676e4edcc83ce6f15
Linus Hennessee <henne519@umn.edu>
2020-04-25 14:47:31 -0500

    f

--- sebastian.js
+++ sebastian.js
@@ -1,34 +1,34 @@
 function abo() {
-  var a = document.getElementbyId("abo");
-  var v = document.getElementbyId("vid");
-  var g = document.getElementbyId("gal");
+  var a = document.getElementbyId("abo").style.display = "block";
+  var v = document.getElementbyId("vid").style.display = "none";
+  var g = document.getElementbyId("gal").style.display = "none";
   a.style.display = "block";
   v.style.display = "none";
   g.style.display = "none";
 }
 
 function vid() {
-  var a = document.getElementbyId("abo");
-  var v = document.getElementbyId("vid");
-  var g = document.getElementbyId("gal");
+  var a = document.getElementbyId("abo").style.display = "none";
+  var v = document.getElementbyId("vid").style.display = "block";
+  var g = document.getElementbyId("gal").style.display = "none";
   a.style.display = "none";
   v.style.display = "block";
   g.style.display = "none";
 }
 
 function gal() {
-  var a = document.getElementbyId("abo");
-  var v = document.getElementbyId("vid");
-  var g = document.getElementbyId("gal");
+  var a = document.getElementbyId("abo").style.display = "none";
+  var v = document.getElementbyId("vid").style.display = "none";
+  var g = document.getElementbyId("gal").style.display = "block";
   a.style.display = "none";
   v.style.display = "none";
   g.style.display = "block";
 }
 
 function begin() {
-  var a = document.getElementbyId("abo");
-  var v = document.getElementbyId("vid");
-  var g = document.getElementbyId("gal");
+  var a = document.getElementbyId("abo").style.display = "none";
+  var v = document.getElementbyId("vid").style.display = "none";
+  var g = document.getElementbyId("gal").style.display = "none";
   a.style.display = "none";
   v.style.display = "none";
   g.style.display = "none";
