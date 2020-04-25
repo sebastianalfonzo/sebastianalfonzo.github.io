b1e8ac90695f415d27a3d0aa3580aef2a0ba8dd4
Linus Hennessee <henne519@umn.edu>
2020-04-25 15:16:28 -0500

    f

--- sebastian.js
+++ sebastian.js
@@ -2,25 +2,18 @@ function abo() {
   document.getElementbyId("abo").style.display = "block";
   document.getElementbyId("vid").style.display = "none";
   document.getElementbyId("gal").style.display = "none";
+  alert("end")
 }
 
 function vid() {
+  alert("begin")
   document.getElementbyId("abo").style.display = "none";
   document.getElementbyId("vid").style.display = "block";
   document.getElementbyId("gal").style.display = "none";
-
 }
 
 function gal() {
   document.getElementbyId("abo").style.display = "none";
   document.getElementbyId("vid").style.display = "none";
   document.getElementbyId("gal").style.display = "block";
-
-}
-
-function begin() {
-  document.getElementbyId("abo").style.display = "none";
-  document.getElementbyId("vid").style.display = "none";
-  document.getElementbyId("gal").style.display = "none";
-  alert("hmm?")
 }
