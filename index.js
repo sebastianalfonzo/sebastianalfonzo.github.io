4823f170c199450ae9a5a6703d8eb175c0207999
Linus Hennessee <linus.hennessee@gmail.com>
2020-08-02 22:29:24 -0500

    refactored javascript, and added specific pages for vids

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 function about() {
+  hideElements();
   document.getElementById("about").style.display = "block";
-  document.getElementById("videography").style.display = "none";
-  document.getElementById("gallery").style.display = "none";
-  document.getElementById("welcome").style.display = "none";
-  document.getElementById("sekrit-zone").style.display = "none";
   document.getElementById("aboutButton").style.color = "#3D7AB8";
-  document.getElementById("vidButton").style.color = "#122A68";
-  document.getElementById("galButton").style.color = "#122A68";
 }
 
 function videography() {
-  document.getElementById("about").style.display = "none";
+  hideElements();
   document.getElementById("videography").style.display = "block";
-  document.getElementById("gallery").style.display = "none";
-  document.getElementById("welcome").style.display = "none";
-  document.getElementById("sekrit-zone").style.display = "none";
-  document.getElementById("aboutButton").style.color = "#122A68";
   document.getElementById("vidButton").style.color = "#3D7AB8";
-  document.getElementById("galButton").style.color = "#122A68";
 }
 
 function gallery() {
-  document.getElementById("about").style.display = "none";
-  document.getElementById("videography").style.display = "none";
+  hideElements();
   document.getElementById("gallery").style.display = "block";
-  document.getElementById("welcome").style.display = "none";
-  document.getElementById("sekrit-zone").style.display = "none";
-  document.getElementById("aboutButton").style.color = "#122A68";
-  document.getElementById("vidButton").style.color = "#122A68";
   document.getElementById("galButton").style.color = "#3D7AB8";
 }
 
 function welcome() {
-  document.getElementById("about").style.display = "none";
-  document.getElementById("videography").style.display = "none";
-  document.getElementById("gallery").style.display = "none";
+  hideElements();
   document.getElementById("welcome").style.display = "block";
-  document.getElementById("sekrit-zone").style.display = "none";
-  document.getElementById("aboutButton").style.color = "#122A68";
-  document.getElementById("vidButton").style.color = "#122A68";
-  document.getElementById("galButton").style.color = "#122A68";
 }
 
 function sekrit() {
+  hideElements();
+  document.getElementById("sekrit-zone").style.display = "block";
+}
+
+function hideShow(element) {
+  hideElements();
+  document.getElementById(element).style.display = "block";
+  
+  if (element === "videography") {
+    document.getElementById("vidButton").style.color = "#3D7AB8";
+  }
+  else if (element === "gallery") {
+    document.getElementById("galButton").style.color = "#3D7AB8";
+  }
+  else if (element === "about") {
+    document.getElementById("aboutButton").style.color = "#3D7AB8";
+  }
+}
+
+function hideElements() {
   document.getElementById("about").style.display = "none";
   document.getElementById("videography").style.display = "none";
   document.getElementById("gallery").style.display = "none";
   document.getElementById("welcome").style.display = "none";
-  document.getElementById("sekrit-zone").style.display = "block";
+  document.getElementById("sekrit-zone").style.display = "none";
+  document.getElementById("vid1").style.display = "none";
+  document.getElementById("vid2").style.display = "none";
+  document.getElementById("gal1").style.display = "none";
+  document.getElementById("gal2").style.display = "none";
+  document.getElementById("gal3").style.display = "none";
+  document.getElementById("gal4").style.display = "none";
   document.getElementById("aboutButton").style.color = "#122A68";
   document.getElementById("vidButton").style.color = "#122A68";
   document.getElementById("galButton").style.color = "#122A68";
+
 }
