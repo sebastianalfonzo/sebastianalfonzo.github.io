91cb794b8200ac431be83a656af5754966f4fe50
Linus Hennessee <linus.hennessee@gmail.com>
2023-01-21 21:18:20 -0600

    jan 21 2023 first commit of changes making things better on mobile. About page, gallery, and other stuff. THis is while sebastian is making IGT music vid

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 function resizeIframe(obj) {
     console.log(obj.style.width);
 }
+
+function vidHideShowIndexAbout() {
+  var vid = document.getElementById("vidButtons");
+  var gal = document.getElementById("galButtons");
+  if (vid.style.display === "none") {
+    vid.style.display = "block";
+    gal.style.display = "none";
+  } else {
+    vid.style.display = "none";
+  }
+}
+
+function galHideShowIndexAbout() {
+  var gal = document.getElementById("galButtons");
+  var vid = document.getElementById("vidButtons");
+  if (gal.style.display === "none") {
+    gal.style.display = "block";
+    vid.style.display = "none";
+  } else {
+    gal.style.display = "none";
+  }
+}
+
+  function vidHideShow() {
+    var vid = document.getElementById("vidButtons");
+    var gal = document.getElementById("galButtons");
+    if (vid.style.display === "none") {
+      vid.style.display = "block";
+    } else {
+      vid.style.display = "none";
+    }
+  }
+
+  function galHideShow() {
+    var gal = document.getElementById("galButtons");
+    var vid = document.getElementById("vidButtons");
+    if (gal.style.display === "none") {
+      gal.style.display = "block";
+    } else {
+      gal.style.display = "none";
+    }
+}
